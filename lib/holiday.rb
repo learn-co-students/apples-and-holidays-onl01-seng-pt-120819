1f0984f7c4e52c70dca2e8d6bd91d0d39a189841
kowalix007 <kowalix007@gmail.com>
2019-12-21 16:57:40 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -20,29 +20,44 @@ def second_supply_for_fourth_of_july(holiday_hash)
   # return the second element in the 4th of July array
 end
 
+def second_supply_for_fourth_of_july(holiday_supplies)
+  holiday_supplies[:summer][:fourth_of_july][1]
+end
+
 def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
-
+holiday_supplies[:winter][:christmas] = supply
+holiday_supplies[:winter][:new_years] = supply
 end
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
+holiday_hash[:spring][:memorial_day]= supply
 
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
   # remember to return the updated hash
-
+ holiday_hash[season][holiday_name] = supply_array 
+ holiday_supplies
+ #binding.pry
 end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-
+holiday_hash[:winter][:christmas]
+holiday_hash[:winter][:new_years]
+new_array = []
+new_array << holiday_hash[:winter][:christmas]
+new_array.push (holiday_hash[:winter][:new_years])
+new_array.flatten
+new array
+# iterate over holiday hash (winter key) we will be able to access key and values of nested hash
 end
 
 def all_supplies_in_holidays(holiday_hash)
@@ -59,9 +74,12 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+ bbq_season = Array.new
+  holiday_hash.each do |season, holidays|
+  holiday.each do |holiday_name, supply|
+end 
 end
-
+end
 
 
 
